80d377bed7047ea58e59ff6558dfccbeeb16bd6a
Rameshwar Singh <112180957+rameshwar-gkp@users.noreply.github.com>
2024-11-26 15:42:42 +0530

    Add files via upload
    boxpatternfile

---
+++ Loop Constraint/boxnumberpattern.c
@@ -0,0 +1,28 @@
+
+int main(){
+    int n=5;
+    int i,j,k;
+    for(i=0;i<=n-1;i++)
+    {
+        for(j=0;j<i;j++)
+            printf("%d",n-j);
+        for(k=0;k<n*2-i*2-1;k++)
+            printf("%d",n-i);
+        for(j=i-1;j>=0;j--)
+            printf("%d",n-j);
+        printf("\n");
+
+    }
+    for(i=n-2;i>=0;i--)
+    {
+        for(j=0;j<i;j++)
+            printf("%d",n-j);
+        for(k=0;k<n*2-i*2-1;k++)
+            printf("%d",n-i);
+        for(j=i-1;j>=0;j--)
+            printf("%d",n-j);
+        printf("\n");
+
+    }
+
+}
